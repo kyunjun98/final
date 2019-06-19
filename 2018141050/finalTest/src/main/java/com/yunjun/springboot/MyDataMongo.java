aae37efca9d159e03e4d749a7e301dca60354959
kyunjun <kyunjun98@naver.com>
2019-06-20 01:30:21 +0900

    필드 선언

--- 2018141050/finalTest/src/main/java/com/yunjun/springboot/MyDataMongo.java
+++ 2018141050/finalTest/src/main/java/com/yunjun/springboot/MyDataMongo.java
@@ -5,5 +5,16 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 
 public class MyDataMongo {
-
+	@Id
+	private String id;
+	
+	private String idNum;
+	private String name;
+	private int phone1;
+	private int phone2;
+	private int phone3;
+	private String pos;
+	private String email;
+	private String sex;
+	private String memo;
 }
