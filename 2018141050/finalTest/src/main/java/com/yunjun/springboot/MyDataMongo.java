5785a7642663e62f6928c322d664c8ce401f5817
kyunjun <kyunjun98@naver.com>
2019-06-20 01:41:03 +0900

    MyDataMongo 생성장 구현 및
    private int를 아래로 내려서 가독성 높임

--- 2018141050/finalTest/src/main/java/com/yunjun/springboot/MyDataMongo.java
+++ 2018141050/finalTest/src/main/java/com/yunjun/springboot/MyDataMongo.java
@@ -10,11 +10,27 @@ public class MyDataMongo {
 	
 	private String idNum;
 	private String name;
-	private int phone1;
-	private int phone2;
-	private int phone3;
 	private String pos;
 	private String email;
 	private String sex;
 	private String memo;
+	private int phone1;
+	private int phone2;
+	private int phone3;
+	
+	
+	public MyDataMongo(String idNum, String name, String pos, String email, String sex, String memo, 
+					   int phone1, int phone2, int phone3) {
+		this.idNum = idNum;
+		this.name = name;
+		this.pos = pos;
+		this.email = email;
+		this.sex = sex;
+		this.memo = memo;
+		this.phone1 = phone1;
+		this.phone2 = phone2;
+		this.phone3 = phone3;
+	}
+	
 }
+
