a7efe66acf20a9c7622b538e6dc80921518dc932
kyunjun <kyunjun98@naver.com>
2019-06-20 02:03:29 +0900

    get/set

--- 2018141050/finalTest/src/main/java/com/yunjun/springboot/MyDataMongo.java
+++ 2018141050/finalTest/src/main/java/com/yunjun/springboot/MyDataMongo.java
@@ -31,6 +31,86 @@ public class MyDataMongo {
 		this.phone2 = phone2;
 		this.phone3 = phone3;
 	}
-	
+
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getIdNum() {
+		return idNum;
+	}
+
+	public void setIdNum(String idNum) {
+		this.idNum = idNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPos() {
+		return pos;
+	}
+
+	public void setPos(String pos) {
+		this.pos = pos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public int getPhone1() {
+		return phone1;
+	}
+
+	public void setPhone1(int phone1) {
+		this.phone1 = phone1;
+	}
+
+	public int getPhone2() {
+		return phone2;
+	}
+
+	public void setPhone2(int phone2) {
+		this.phone2 = phone2;
+	}
+
+	public int getPhone3() {
+		return phone3;
+	}
+
+	public void setPhone3(int phone3) {
+		this.phone3 = phone3;
+	}
 }
 
